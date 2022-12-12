b158dbcb50b55adececc651c709dc239e2bf6219
shaneam12 <44936734+shaneam12@users.noreply.github.com>
2022-12-11 22:07:23 -0800

    Add files via upload

---
+++ DataClasses.py
@@ -0,0 +1,237 @@
+#------------------------------------------#
+# Title: Data Classes
+# Desc: A Module for Data Classes
+# Change Log: (Who, When, What)
+# DBiesinger, 2030-Jan-01, Created File
+# DBiesinger, 2030-Jan-02, Modified to add Track class, added methods to CD class to handle tracks
+# Shane McLeod, 2022-Dec-12, Added code for TODOs
+#------------------------------------------#
+
+if __name__ == '__main__':
+    raise Exception('This file is not meant to run by itself')
+
+class Track():
+    """Stores Data about a single Track:
+
+    properties:
+        position: (int) with Track position on CD / Album
+        title: (str) with Track title
+        length: (str) with length / playtime of Track
+    methods:
+        get_record() -> (str)
+
+    """
+
+    # -- Constructor -- #
+    def __init__(self, pos, tit, len):
+        # -- Attributes -- #
+        try:
+            self.__pos = int(pos)
+            self.__tit = str(tit)
+            self.__len = str(len)
+        except:
+            raise Exception('Error: Unable to set initial values')
+    # -- Properties -- #
+    @property
+    def pos(self):
+        return self.__pos
+    
+    @pos.setter
+    def pos(self, input):
+        try:
+            self.__pos = int(input)
+        except:
+            raise Exception('Error: Make sure Track Posistion is entered as an Integer')
+
+    @property
+    def tit(self):
+        return self.__tit
+    
+    @tit.setter
+    def tit(self, input):
+        try:
+            self.__tit = str(input)
+        except:
+            raise Exception('Error: Make sure Title is entered as a String')
+
+    @property
+    def len(self):
+        return self.__len
+    
+    @len.setter
+    def len(self, input):
+        try:
+            self.__len = str(input)
+        except:
+            raise Exception('Error: Make sure Length is entered as a String')
+
+    # -- Methods -- #
+    def __str__(self):
+        """Returns Track details as formatted string"""
+        return '{}\t {} (Playback Time: {})'.format(self.pos, self.tit, self.len)
+
+    def get_record(self) -> str:
+        """Returns: Track record formatted for saving to file"""
+        return '{},{},{}\n'.format(self.pos, self.tit, self.len)
+
+
+class CD:
+    """Stores data about a CD / Album:
+
+    properties:
+        cd_id: (int) with CD  / Album ID
+        cd_title: (string) with the title of the CD / Album
+        cd_artist: (string) with the artist of the CD / Album
+        cd_tracks: (list) with track objects of the CD / Album
+    methods:
+        get_record() -> (str)
+        add_track(track) -> None
+        rmv_track(int) -> None
+        get_tracks() -> (str)
+        get_long_record() -> (str)
+
+    """
+    # -- Constructor -- #
+    def __init__(self, cd_id: int, cd_title: str, cd_artist: str) -> None:
+        """Set ID, Title and Artist of a new CD Object"""
+        #    -- Attributes  -- #
+        try:
+            self.__cd_id = int(cd_id)
+            self.__cd_title = str(cd_title)
+            self.__cd_artist = str(cd_artist)
+            self.__tracks = []
+        except Exception as e:
+            raise Exception('Error setting initial values:\n' + str(e))
+
+    # -- Properties -- #
+    # CD ID
+    @property
+    def cd_id(self):
+        return self.__cd_id
+
+    @cd_id.setter
+    def cd_id(self, value):
+        try:
+            self.__cd_id = int(value)
+        except Exception:
+            raise Exception('ID needs to be Integer')
+
+    # CD title
+    @property
+    def cd_title(self):
+        return self.__cd_title
+
+    @cd_title.setter
+    def cd_title(self, value):
+        try:
+            self.__cd_title = str(value)
+        except Exception:
+            raise Exception('Title needs to be String!')
+
+    # CD artist
+    @property
+    def cd_artist(self):
+        return self.__cd_artist
+
+    @cd_artist.setter
+    def cd_artist(self, value):
+        try:
+            self._cd_artist = str(value)
+        except Exception:
+            raise Exception('Artist needs to be String!')
+
+    # CD tracks
+    @property
+    def cd_tracks(self):
+        return self.__tracks
+
+    # -- Methods -- #
+    def __str__(self):
+        """Returns: CD details as formatted string"""
+        return '{:>2}\t{} (by: {})'.format(self.cd_id, self.cd_title, self.cd_artist)
+
+    def get_record(self):
+        """Returns: CD record formatted for saving to file"""
+        return '{},{},{}\n'.format(self.cd_id, self.cd_title, self.cd_artist)
+
+    def add_track(self, track: Track) -> None:
+        """Adds a track to the CD / Album
+
+
+        Args:
+            track (Track): Track object to be added to CD / Album.
+
+        Returns:
+            None.
+
+        """
+        self.__tracks.append(track)
+        self.__sort_tracks()
+
+    def rmv_track(self, track_id: int) -> None:
+        """Removes the track identified by track_id from Album
+
+
+        Args:
+            track_id (int): ID of track to be removed.
+
+        Returns:
+            None.
+
+        """
+        try:
+            TrackNum = int(track_id) - 1
+        except Exception:
+            raise Exception('ID must be an integer')
+
+        self.__tracks.pop(TrackNum)
+        self.__sort_tracks()
+
+    def __sort_tracks(self):
+        """Sorts the tracks using Track.position. Fills blanks with None"""
+        n = len(self.__tracks)
+        for track in self.__tracks:
+            if (track is not None) and (n < track.pos):
+                n = track.pos
+        tmp_tracks = [None] * n
+        for track in self.__tracks:
+            if track is not None:
+                tmp_tracks[track.pos - 1] = track
+        self.__tracks = tmp_tracks
+
+    def get_tracks(self) -> str:
+        """Returns a string list of the tracks saved for the Album
+
+        Raises:
+            Exception: If no tracks are saved with album.
+
+        Returns:
+            result (string):formatted string of tracks.
+
+        """
+        self.__sort_tracks()
+        if len(self.__tracks) < 1:
+            raise Exception('No tracks saved for this Album')
+        result = ''
+        for track in self.__tracks:
+            if track is None:
+                result += 'No Information for this track\n'
+            else:
+                result += str(track) + '\n'
+        return result
+
+    def get_long_record(self) -> str:
+        """gets a formatted long record of the Album: Album information plus track details
+
+
+        Returns:
+            result (string): Formatted information about ablum and its tracks.
+
+        """
+        result = self.get_record() + '\n'
+        result += self.get_tracks() + '\n'
+        return result
+
+
+
+
